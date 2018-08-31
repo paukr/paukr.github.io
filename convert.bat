d00c84a0dcc54ee3c180e391dac0da4c8a7ee677
Paul <paul.kroener@gmail.com>
2018-08-31 20:59:20 +0200

    srcset bat ini

---
+++ convert.bat
@@ -0,0 +1,3 @@
+i_view64.exe "%1\*.JPG" /advancedbatch /resize=(576,0) /resample /aspectratio /jpgq=90 /convert="%2\$N_S.JPG"
+i_view64.exe "%1\*.JPG" /advancedbatch /resize=(768,0) /resample /aspectratio /jpgq=90 /convert="%2\$N_M.JPG"
+i_view64.exe "%1\*.JPG" /advancedbatch /resize=(992,0) /resample /aspectratio /jpgq=90 /convert="%2\$N_L.JPG"
